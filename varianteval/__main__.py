41ff954c66e3073eae427409d925e5773bd21d94
Kiran V Garimella <kiran@broadinstitute.org>
2022-03-22 02:06:57 -0400

    Example for first test

--- varianteval/__main__.py
+++ varianteval/__main__.py
@@ -6,23 +6,22 @@ import click_log
 import sys
 from datetime import datetime
 
-import varianteval
 from . import __version__
 
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("version")
 click_log.basic_config(logger)
 logger.handlers[0].formatter = logging.Formatter(
     "[%(levelname)s %(asctime)s %(name)8s] %(message)s", "%Y-%m-%d %H:%M:%S"
 )
 
 
-@click.group(name="varianteval")
-def main_entry():
+@click.group()
+def cli():
     logger.info("Invoked via: varianteval %s", " ".join(sys.argv))
 
 
-@main_entry.command()
+@cli.command()
 @click_log.simple_verbosity_option(logger)
 def version():
     """Print the version of varianteval."""
@@ -30,4 +29,4 @@ def version():
 
 
 if __name__ == "__main__":
-    main_entry()
+    cli()
---
+++ varianteval/record.py
@@ -0,0 +1,5 @@
+import sys
+
+
+def add(a, b):
+    return a + b
