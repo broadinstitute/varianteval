c85aa62f2e4e32c95803c1409b64dd8f19443cca
crohlicek <chris@rohlicek.com>
2022-06-02 11:35:30 -0400

    Put SVLEN filtering logic back, constructing SVLEN from record start/stop fields

--- varianteval/scripts/build_igv_batch_script.py
+++ varianteval/scripts/build_igv_batch_script.py
@@ -7,8 +7,8 @@ import argparse
 
 
 def generate_script(input_vcf, bam_path, output_batch_script_path, igv_screenshot_dir, min_svlen,
-                    max_svlen, trg_supp_vec=None, genome='hg38', colorby_ins_size=True,
-                    groupby_pair_orientation=True, viewaspairs=True, screenshot_margin=2000):
+                    max_svlen, genome='hg38', colorby_ins_size=True, groupby_pair_orientation=True,
+                    viewaspairs=True):
     """
     method to generate a batch script for an input vcf
     """
@@ -22,26 +22,26 @@ def generate_script(input_vcf, bam_path, output_batch_script_path, igv_screensho
     write_first_time = True
     input_vcf_file = VariantFile(input_vcf)
     for rec in input_vcf_file.fetch():
-        # if a target supp vec is given, then skip this record if it doesn't match
-        if trg_supp_vec:
-            if rec.info['SUPP_VEC'] != trg_supp_vec:
-                continue
-        start_pos = str(rec.start - screenshot_margin)
-        end_pos = str(rec.stop + screenshot_margin)
-        svtype = rec.info['SVTYPE']
-        out.write('goto ' + rec.chrom + ':' + start_pos + '-' + end_pos + '\n')
-        if write_first_time:
-            if colorby_ins_size:
-                out.write('colorby INSERT_SIZE\n')
-            if groupby_pair_orientation:
-                out.write('group PAIR_ORIENTATION\n')
-            if viewaspairs:
-                out.write('viewaspairs\n')
-            out.write('maxPanelHeight 1000\n')
-            out.write('snapshotDirectory ' + igv_screenshot_dir + '\n')
-            write_first_time = False
-        out.write('collapse\n')
-        out.write(f'snapshot {rec.chrom}_{start_pos}_{end_pos}_{svtype}.png\n')
+        svlen = rec.stop - rec.start
+        if min_svlen <= svlen <= max_svlen:
+            # setting the margin on either side of the interval to svlen/10
+            screenshot_margin = svlen // 10
+            start_pos = str(rec.start - screenshot_margin)
+            end_pos = str(rec.stop + screenshot_margin)
+            svtype = rec.info['SVTYPE']
+            out.write('goto ' + rec.chrom + ':' + start_pos + '-' + end_pos + '\n')
+            if write_first_time:
+                if colorby_ins_size:
+                    out.write('colorby INSERT_SIZE\n')
+                if groupby_pair_orientation:
+                    out.write('group PAIR_ORIENTATION\n')
+                if viewaspairs:
+                    out.write('viewaspairs\n')
+                out.write('maxPanelHeight 1000\n')
+                out.write('snapshotDirectory ' + igv_screenshot_dir + '\n')
+                write_first_time = False
+            out.write('collapse\n')
+            out.write(f'snapshot {rec.chrom}_{start_pos}_{end_pos}_{svtype}.png\n')
     out.close()
 
 
@@ -59,8 +59,6 @@ if __name__ == '__main__':
     parser.add_argument('--colorby_insert_size', type=bool, default=True, help='Adds IGV colorby INSERT SIZE')
     parser.add_argument('--groupby_pair_orientation', type=bool, default=True, help='Adds groupby PAIR ORIENTATION')
     parser.add_argument('--viewaspairs', type=bool, default=True, help='Adds viewaspairs')
-    parser.add_argument('--screenshot_margin', type=int, default=2000, help='Margin to be added to either side of event'
-                                                                            'interval for IGV screenshot (set in bp)')
     args = parser.parse_args()
 
     if args.igv_screenshot_dir is None:
@@ -72,4 +70,4 @@ if __name__ == '__main__':
                     igv_screenshot_dir, args.min_svlen, args.max_svlen, genome=args.ref_genome,
                     colorby_ins_size=args.colorby_insert_size,
                     groupby_pair_orientation=args.groupby_pair_orientation,
-                    viewaspairs=args.viewaspairs, screenshot_margin=args.screenshot_margin)
+                    viewaspairs=args.viewaspairs)
