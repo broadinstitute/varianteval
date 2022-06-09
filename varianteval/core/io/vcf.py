0b757eec73608455fdc4a905259108ef58b3c13b
Fabio Cunial <fcunial@wm34a-058.broadinstitute.org>
2022-06-09 09:49:17 -0400

    fixing comments on pull request #8

--- varianteval/core/io/vcf.py
+++ varianteval/core/io/vcf.py
@@ -32,26 +32,26 @@ def get_sv_len(pysam_vcf_record):
 
 def record2sv(record):
     """Converts the VCF record into the interval SV event object"""
-    sv_type = get_sv_type(record)
-    sv_len = get_sv_len(record)
-    # TODO: construct the internal representation
-    #return SV(sv_type)
-    return None
+    # sv_type = get_sv_type(record)
+    # sv_len = get_sv_len(record)
+    # # TODO: construct the internal representation
+    # return SV(sv_type)
+    # return None
 
 
 def filter_record(record, filter_funcs=None):
     """Applies a set of filters to the record"""
-    if filter_funcs is None:
-        filter_funcs = []
-    pass
+    # if filter_funcs is None:
+    #     filter_funcs = []
+    # pass
 
 
 def vcf_iter(vcf_fname, filter_funcs=None):
-    vcf_file = VariantFile(vcf_fname)
-    for rec in vcf_file.fetch():
-        if not filter_record(rec, filter_funcs):
-            # TODO: handle multi-line events
-            yield record2sv(rec)
+    # vcf_file = VariantFile(vcf_fname)
+    # for rec in vcf_file.fetch():
+    #     if not filter_record(rec, filter_funcs):
+    #         # TODO: handle multi-line events
+    #         yield record2sv(rec)
     # consolidate multi-line calls
 
 
@@ -65,7 +65,7 @@ def vcf2callset(vcf_fname, filter_funcs=None):
     #         callset.add(record2sv(rec))
     # # TODO: consolidate multi-line calls
     # return callset
-    return None
+    #return None
 
 
 def svs2vcf(vcf_fname, sv_callset):
